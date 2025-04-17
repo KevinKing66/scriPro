b50fe81a22d586ef3f72c6e4ea3867c5f8804131
KevinKing66 <kevinvallenato2002@gmail.com>
2025-04-17 16:06:51 -0500

    Se agg navegacion funcional

--- src/app/features/projects/components/project-card/project-card.component.ts
+++ src/app/features/projects/components/project-card/project-card.component.ts
@@ -1,15 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { Projects } from '../../model/project.model';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'project-card',
-    imports: [],
+    imports: [RouterModule],
     templateUrl: './project-card.component.html',
     styleUrl: './project-card.component.css'
 })
 export class ProjectCardComponent {
   @Input() project: Projects = {
-    uuid: '',
+    uuid: 'uuid',
     name: 'Proyecto De Investigación',
     description: 'Laborum labore adipisicing culpa ut occaecat eiusmod Lorem reprehenderit occaecat ipsum. Incididunt exercitation aute quis cupidatat sint cillum non non reprehenderit. Eiusmod elit esse consectetur non. Mollit mollit excepteur qui velit est ullamco consequat.',
     creationDateTime: new Date(),
