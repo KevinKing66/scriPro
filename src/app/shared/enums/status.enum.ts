6f1326ad522fe86becb1e1e12a35a400676e9733
KevinKing66 <kevinvallenato2002@gmail.com>
2025-04-18 16:43:56 -0500

    Nuevos enums

---
+++ src/app/shared/enums/status.enum.ts
@@ -0,0 +1,11 @@
+export enum UserStatus {
+  ACTIVE = 'Activo',
+  DESACTIVE = 'Desactivado'
+}
+
+
+export enum ProjectStatus {
+  ACTIVE = 'Activo',
+  COMPLETED = 'Completado',
+  PAUSED = 'En pausa'
+}
