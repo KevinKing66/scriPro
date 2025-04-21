61eb705732d40d6c58d43930f8b4eefcf6dae229
KevinKing66 <kevinvallenato2002@gmail.com>
2025-04-20 19:32:47 -0500

    Se agg tipado

--- src/app/features/authentication/pages/register.page.ts
+++ src/app/features/authentication/pages/register.page.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { RegisterUserFormComponent } from '../components/register-user-form/register-user-form.component';
 import { AuthService } from '../service/auth.service';
+import { User } from '../models/user.model';
 
 @Component({
   standalone: true,
@@ -15,7 +16,7 @@ import { AuthService } from '../service/auth.service';
 export class RegisterPage {
   constructor(private authService: AuthService, private router: Router) { }
 
-  register(data: any) {
+  register(data: User) {
     this.authService.register(data).subscribe({
       next: () => this.router.navigate(['/auth/login']),
       error: err => console.error('Registration failed', err)
