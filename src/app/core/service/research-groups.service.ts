e46acb9072bcb4267e8f4ed37ae4a4435b08f2f2
KevinKing66 <kevinvallenato2002@gmail.com>
2025-04-22 23:48:35 -0500

    Se agg servicios para crud

--- src/app/core/service/research-groups.service.ts
+++ src/app/core/service/research-groups.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ResearchGroup } from '../../../../../../backend/scri-pro/src/research-groups/entities/research-group.entity';
+import { environment } from '../../../enviroments/enviroment';
+import { HttpClient } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResearchGroupsService {
+  private readonly backendUrl = environment.url;
+  private readonly apiUrl = this.backendUrl + '/research-groups';
+  isRequiredHttps = false;
 
-  constructor() { }
+  constructor(private http: HttpClient, private cookieService: CookieService) { }
+
+  findAll(): Observable<ResearchGroup[]> {
+    return this.http.get<ResearchGroup[]>(`${this.apiUrl}/research-groups`);
+  }
+
+  find(code: string): Observable<ResearchGroup> {
+    return this.http.get<ResearchGroup>(`${this.apiUrl}/research-groups/${code}`);
+  }
+  create(researchGroup: ResearchGroup): Observable<ResearchGroup> {
+    return this.http.post<ResearchGroup>(`${this.apiUrl}/research-groups`, researchGroup);
+  }
+  update(code: string, researchGroup: ResearchGroup): Observable<ResearchGroup> {
+    return this.http.patch<ResearchGroup>(`${this.apiUrl}/research-groups/${code}`, researchGroup);
+  }
 }
