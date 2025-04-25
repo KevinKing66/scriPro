7e354c38fd35c25de9674ae6bfe48e0b301af350
KevinKing66 <kevinvallenato2002@gmail.com>
2025-04-25 02:34:27 -0500

    Se agrega nueva validacion

--- src/app/features/authentication/components/register-user-form/register-user-form.component.ts
+++ src/app/features/authentication/components/register-user-form/register-user-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, Validators, FormGroup, FormControl, FormsModule } from '@angular/forms';
 import { User } from '../../models/user.model';
@@ -9,6 +9,7 @@ import { UserStatus } from '../../../../shared/enums/status.enum';
 import { DocumentTypes } from '../../../../shared/enums/document-type.enum';
 import { UserStatusLabelPipe } from '../../../../shared/pipes/user-status-label.pipe';
 import { ResearchGroup } from '../../../projects/model/project.model';
+import { ResearchGroupsService } from '../../../../core/service/research-groups.service';
 
 @Component({
   selector: 'app-register-user-form',
@@ -17,7 +18,7 @@ import { ResearchGroup } from '../../../projects/model/project.model';
   styleUrls: ['./register-user-form.component.css', '../../../../shared/styles/form.css'],
 })
 // RegisterUserFormComponent
-export class RegisterUserFormComponent {
+export class RegisterUserFormComponent implements OnInit {
   @Input() state: "FREE" | "LOADING" | "ERROR" | "SUCCESS" = "FREE";
   @Output() submitted = new EventEmitter<User>();
 
@@ -32,19 +33,29 @@ export class RegisterUserFormComponent {
 
   researchGroups: ResearchGroup[] = [];
 
+  ngOnInit() {
+    this.loadResearchGroups();
+  }
+
+
+  private researchGroupsService = inject(ResearchGroupsService);
   private formBuilder = inject(FormBuilder);
 
   form = new FormGroup({
     name: this.formBuilder.control('', Validators.required),
     lastName: this.formBuilder.control('', Validators.required),
     email: this.formBuilder.control('', [Validators.required, Validators.email]),
-    password: this.formBuilder.control('', Validators.required),
+    password: this.formBuilder.control('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/)
+    ]),
     code: this.formBuilder.control('', Validators.required),
     docNum: this.formBuilder.control('', Validators.required),
     docType: this.formBuilder.control(this.documentTypeKeys[0], Validators.required),
-    phone: this.formBuilder.control('', Validators.required),
+    phone: this.formBuilder.control('', [Validators.required, Validators.pattern(/^\+?\d{10,15}$/)]),
     role: this.formBuilder.control(this.roleKeys[0], Validators.required),
-    // researchGroupId: this.formBuilder.control(0, Validators.required),
+    researchGroupId: this.formBuilder.control(0, Validators.required),
     status: this.formBuilder.control('ACTIVE', Validators.required),
   });
 
@@ -56,5 +67,33 @@ export class RegisterUserFormComponent {
     } else {
       this.form.markAllAsTouched();
     }
+  }
+
+  loadResearchGroups() {
+    this.researchGroupsService.findAll().subscribe({
+      next: (data: any[]) => {
+        this.researchGroups = data.map(item => ({
+          code: item.code,
+          name: item.name,
+          description: item.description,
+          faculty: item.faculty,
+          creationDate: item.creationDate || '',
+          admin: item.admin,
+          status: item.status,
+          knowledgeArea: item.knowledgeArea,
+          contactEmail: item.contactEmail,
+          contactPhone: item.contactPhone,
+        }));
+      },
+      error: (err: any) => {
+        console.log("Error: ", err);
+        this.state = "ERROR";
+        setTimeout(() => {
+          this.state = "FREE";
+        }, 15000);
+        this.errorMsg = err.error.message || err.error || err.message || err;
+        console.error(err);
+      }
+    });
   }
 }
