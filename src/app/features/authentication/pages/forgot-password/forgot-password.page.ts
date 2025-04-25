6a961eb5d9d6f4570cc512520f5879d334c2c6ea
KevinKing66 <kevinvallenato2002@gmail.com>
2025-04-25 02:35:14 -0500

    Se crea pagina usando el formulario de olvide contraseña

---
+++ src/app/features/authentication/pages/forgot-password/forgot-password.page.ts
@@ -0,0 +1,42 @@
+import { Component, inject } from '@angular/core';
+import { ForgotPasswordFormComponent } from '../../components/forgot-password-form/forgot-password-form.component';
+import { AuthService } from '../../../../core/service/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ChangePassword } from '../../../../../../../../backend/scri-pro/src/auth/dto/change-password.dto';
+import { ForgotPassword } from '../../../../../../../../backend/scri-pro/src/auth/dto/forgot-password.dto';
+
+@Component({
+  selector: 'app-forgot-password.page',
+  imports: [ForgotPasswordFormComponent],
+  templateUrl: './forgot-password.page.html',
+  styleUrl: './forgot-password.page.css'
+})
+export class ForgotPasswordPage {
+  state: "FREE" | "LOADING" | "SUCCESS" | "ERROR" = "FREE";
+  errorMsg: string = "";
+
+  private router: Router = inject(Router);
+  private route: ActivatedRoute = inject(ActivatedRoute);
+  private authService: AuthService = inject(AuthService);
+
+  cancel(){
+    this.router.navigate(['/login']);
+  }
+
+  forgotPassword(data: ForgotPassword) {
+    this.state = "LOADING";
+    this.authService.forgotPassword(data).subscribe({
+      next: () => {
+        this.router.navigate(['/login']);
+      },
+      error: err => {
+        console.error('Change password failed', err);
+        this.state = "ERROR";
+        setTimeout(() => {
+          this.state = "FREE";
+        }, 15000);
+        this.errorMsg = err.error.message || err.error || err.message || err;
+      }
+    });
+  }
+}
