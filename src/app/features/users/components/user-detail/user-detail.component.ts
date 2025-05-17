a246d2e5955d8d1c9bc65f95426cce7c9c97e08c
KevinKing66 <kevinvallenato2002@gmail.com>
2025-05-17 14:06:48 -0500

    cambia la manera como se muestra researchGroups, se agg researchGroupsLabel

--- src/app/features/users/components/user-detail/user-detail.component.ts
+++ src/app/features/users/components/user-detail/user-detail.component.ts
@@ -3,10 +3,11 @@ import { User } from '../../../authentication/models/user.model';
 import { RoleLabelPipe } from '../../../../shared/pipes/role-label.pipe';
 import { DocumentTypeLabelPipe } from '../../../../shared/pipes/document-type-label.pipe';
 import { UserStatusLabelPipe } from '../../../../shared/pipes/user-status-label.pipe';
+import { ResearchGroupsLabelPipe } from '../../../../shared/pipes/research-groups-label.pipe';
 
 @Component({
   selector: 'app-user-detail',
-  imports: [RoleLabelPipe, DocumentTypeLabelPipe, UserStatusLabelPipe],
+  imports: [RoleLabelPipe, DocumentTypeLabelPipe, ResearchGroupsLabelPipe, UserStatusLabelPipe],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.css'
 })
