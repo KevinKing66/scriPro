795b2851e92c827ba549b82aedc5dcfb9339bca8
KevinKing66 <kevinvallenato2002@gmail.com>
2025-05-18 18:54:43 -0500

    Se quitan campos que no se deberian cambiar

--- src/app/features/projects/model/update-project.model.ts
+++ src/app/features/projects/model/update-project.model.ts
@@ -1,15 +1,15 @@
 import { Member } from "./create-project.model";
 import { CreateFileDTO } from "./file.model";
 import { simpleResearchGroup } from "./simple-researchGroup.model";
+import { CreateEvidenceDto } from '../../../../../../../backend/scri-pro/src/evidences/dto/create-evidence.dto';
 
-export interface CreateProject {
+export interface UpdateProject {
+  code?: string | undefined;
+  name?: string | undefined;
   image?: CreateFileDTO;
-  name?: string;
-  type?: string;
-  description?: string;
-  creationDateTime?: Date;
-  status?: 'ACTIVE' | 'COMPLETED' | 'PAUSED';
-  members?: Member[];
-  researchGroups?: simpleResearchGroup[];
-  adminId?: Member;
+  members?: Member[] | undefined;
+  evidences?: CreateEvidenceDto[];
+  description?: string | undefined;
+  status?: 'ACTIVE' | 'COMPLETED' | 'PAUSED' | undefined;
+  researchGroups?: { code: string; name: string } | undefined;
 }
