17fa7515f4f3b0856811a4e2b5d8ede755b814e6
KevinKing66 <kevinvallenato2002@gmail.com>
2025-05-18 14:24:26 -0500

    Avances en la edicion de proyectos

--- src/app/features/projects/components/form/project-edit-form/project-edit-form.component.ts
+++ src/app/features/projects/components/form/project-edit-form/project-edit-form.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, inject, Input, OnChanges, Output, SimpleChange
 import { Project } from '../../../model/project.model';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ResearchGroup } from '../../../../research-group/models/research-group.model';
 
 @Component({
   selector: 'app-project-edit-form',
@@ -11,19 +12,23 @@ import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
 })
 export class ProjectEditFormComponent implements OnChanges {
   @Input() project!: Project;
+
+  @Input() state: 'FREE' | 'LOADING' | 'ERROR' | 'SUCCESS' = 'FREE';
+  @Input() errorMsg: string = '';
+  @Input() researchGroups: ResearchGroup[] = [];
+
   @Output() updated = new EventEmitter<Project>();
 
   private fb = inject(FormBuilder);
 
   form: FormGroup = this.fb.group({
-    name: ['', Validators.required],
-    description: ['', Validators.required],
-    imageUrl: [''],
-    status: ['ACTIVE', Validators.required],
-    researchGroups: this.fb.array([]),
-    updatedAt: [new Date()],
-    evidences: this.fb.array([]),
-    members: this.fb.array([]),
+    name: [this.project.name, Validators.required],
+    description: [this.project.description, Validators.required],
+    image: this.fb.group(this.project?.image || {}),
+    status: [this.project.status, Validators.required],
+    researchGroups: [this.project.researchGroups, [Validators.required]],
+    members: this.fb.array(this.project?.members || []),
+    evidences: this.fb.array(this.project?.evidences || []),
   });
 
   ngOnChanges(changes: SimpleChanges) {
--- src/app/features/projects/pages/edit.page.ts
+++ src/app/features/projects/pages/edit.page.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { Project } from '../model/project.model';
 import { ProjectService } from '../service/project.service';
 import { ProjectEditFormComponent } from '../components/form/project-edit-form/project-edit-form.component';
+import { ResearchGroup } from '../../research-group/models/research-group.model';
+import { ResearchGroupsService } from '../../../core/service/research-groups.service';
+import { FloatingButtonComponent } from '../../../shared/components/floating-button/floating-button.component';
 
 @Component({
   standalone: true,
   selector: 'app-project-edit-page',
-  imports: [CommonModule, ProjectEditFormComponent],
+  imports: [CommonModule, ProjectEditFormComponent, FloatingButtonComponent],
   template: `
   <div class="container">
     <h1>Editar Proyecto</h1>
@@ -17,25 +20,33 @@ import { ProjectEditFormComponent } from '../components/form/project-edit-form/p
     @if (project) {
       <app-project-edit-form
         [project]="project"
+        [state]="state" [errorMsg]="errorMsg"
+        [researchGroups]="researchGroups"
         (updated)="onUpdate($event)"
       />
     } @else {
       <p>Cargando proyecto...</p>
     }
+    <floating-button [route]=destiny content="<"></floating-button>
   </div>
   `
 })
 export class ProjectEditPage implements OnInit {
-  project!: Project;
+  private route: ActivatedRoute = inject(ActivatedRoute);
+  private projectService: ProjectService = inject(ProjectService);
+  private researchGroupsService: ResearchGroupsService = inject(ResearchGroupsService);
 
-  constructor(
-    private route: ActivatedRoute,
-    private projectService: ProjectService
-  ) { }
+  destiny: string | string[] = ['/projects'];
+
+  project!: Project;
+  researchGroups: ResearchGroup[] = [];
+
+  state: "FREE" | "LOADING" | "ERROR" | "SUCCESS" = "FREE";
+  errorMsg: string = "";
 
   ngOnInit(): void {
     const _id: string | null = this.route.snapshot.paramMap.get('_id');
-    if(_id) {
+    if (_id) {
       this.projectService.findOne(_id).subscribe((data) => {
         this.project = data;
       });
@@ -43,9 +54,52 @@ export class ProjectEditPage implements OnInit {
   }
 
   onUpdate(updatedProject: Project): void {
-    this.projectService.update(this.project._id, updatedProject).subscribe(() => {
-      // redirige o muestra feedback
-      alert('Proyecto actualizado con éxito.');
+    this.projectService.update(this.project._id, updatedProject).subscribe({
+      next: () => {
+        this.errorMsg = "";
+        this.state = "SUCCESS";
+        setTimeout(() => {
+          this.state = "FREE";
+        }, 8000);
+      },
+      error: err => {
+        console.log("Error: ", err);
+        this.state = "ERROR";
+        setTimeout(() => {
+          this.state = "FREE";
+        }, 15000);
+        this.errorMsg = err.error.message || err.error || err.message || err;
+      },
+    });
+  }
+
+
+
+  loadResearchGroups() {
+    this.researchGroupsService.findAll().subscribe({
+      next: (data: any[]) => {
+        this.researchGroups = data.map(item => ({
+          code: item.code,
+          name: item.name,
+          description: item.description,
+          faculty: item.faculty,
+          creationDate: item.creationDate || '',
+          admin: item.admin,
+          status: item.status,
+          knowledgeArea: item.knowledgeArea,
+          contactEmail: item.contactEmail,
+          contactPhone: item.contactPhone,
+        }));
+      },
+      error: (err: any) => {
+        console.log("Error: ", err);
+        this.state = "ERROR";
+        setTimeout(() => {
+          this.state = "FREE";
+        }, 15000);
+        this.errorMsg = err.error.message || err.error || err.message || err;
+        console.error(err);
+      }
     });
   }
 }
