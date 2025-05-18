e33dc8be9bcb02c006447c9d8823fe82a3df8930
KevinKing66 <kevinvallenato2002@gmail.com>
2025-05-17 19:34:54 -0500

    Cambios en paaginacion

--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -1,16 +1,19 @@
-import { Component, computed, Input, Signal, signal, WritableSignal } from '@angular/core';
-
+import { Component, EventEmitter, Input, Output, computed, signal, WritableSignal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 @Component({
-    selector: 'app-pagination',
-    imports: [CommonModule],
-    templateUrl: './pagination.component.html',
-    styleUrl: './pagination.component.css'
+  selector: 'app-pagination',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pagination.component.html',
+  styleUrl: './pagination.component.css'
 })
 export class PaginationComponent {
   @Input() maxPages: number = 1;
+  @Input() initialPage: number = 0;
+  @Output() pageChange = new EventEmitter<number>();
 
-  page: WritableSignal<number> = signal(0);
+  page: WritableSignal<number> = signal(this.initialPage);
 
   getPage = computed(() => {
     const current = this.page();
@@ -29,23 +32,34 @@ export class PaginationComponent {
 
   changePage(i: number) {
     this.page.set(i);
+    this.pageChange.emit(i);
   }
 
   previous() {
-    if (this.page() > 0) {
-      this.page.update((p) => p - 1);
+    if (this.page() > 1) {
+      this.page.update((p) => {
+        const newPage = p - 1;
+        this.pageChange.emit(newPage);
+        return newPage;
+      });
     }
-  }
-  nextPageExists() {
-    return this.page() < this.maxPages - 1 && this.maxPages > 1;
-  }
-  previousPageExists() {
-    return this.page() > 0 && this.maxPages > 1;
   }
 
   next() {
-    if (this.page() < this.maxPages - 1) {
-      this.page.update((p) => p + 1);
+    if (this.page() < this.maxPages) {
+      this.page.update((p) => {
+        const newPage = p + 1;
+        this.pageChange.emit(newPage);
+        return newPage;
+      });
     }
+  }
+
+  nextPageExists() {
+    return this.page() < this.maxPages && this.maxPages > 1;
+  }
+
+  previousPageExists() {
+    return this.page() > 1 && this.maxPages > 1;
   }
 }
