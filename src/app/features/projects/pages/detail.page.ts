54ca5b7ffef9ad5c280eb78cced13d15c26fbc71
KevinKing66 <kevinvallenato2002@gmail.com>
2025-05-18 18:29:03 -0500

    Mas ajustes

--- src/app/features/projects/pages/detail.page.ts
+++ src/app/features/projects/pages/detail.page.ts
@@ -7,6 +7,8 @@ import { ProjectDetailsComponent } from '../components/others/project-details/pr
 import { ProjectService } from '../service/project.service';
 import { Evidences, Project } from '../model/project.model';
 import { LoadingComponent } from '../../../shared/components/loading/loading.component';
+import { StorageService } from '../../../core/service/storage.service';
+import { Member } from '../model/create-project.model';
 
 @Component({
   standalone: true,
@@ -14,7 +16,7 @@ import { LoadingComponent } from '../../../shared/components/loading/loading.com
   imports: [CommonModule, ProjectDetailsComponent, FloatingButtonComponent, LoadingComponent],
   template: `
     @if(project) {
-      <app-project-details [project]=project></app-project-details>
+      <app-project-details [project]=project [isOwner]=isOwner></app-project-details>
     }
     @else {
       <app-loading></app-loading>
@@ -27,6 +29,9 @@ export class ProjectDetailPage implements OnInit {
   private projectService: ProjectService = inject(ProjectService);
   private router: Router = inject(Router);
   private route: ActivatedRoute = inject(ActivatedRoute);
+  storageService: StorageService = inject(StorageService);
+
+  isOwner: boolean = false;
 
   ngOnInit() {
     this._id = this.route.snapshot.paramMap.get('_id') || '';
@@ -45,11 +50,22 @@ export class ProjectDetailPage implements OnInit {
 
   find() {
     this.projectService.findOne(this._id).subscribe((res: Project) => {
-      if(!res) {
+      if (!res) {
         this.router.navigate(['/projects']);
         return;
       }
       this.project = res;
+      this.refreshIsOwner();
     });
+  }
+
+  refreshIsOwner() {
+    const user = this.storageService.getSession();
+    const owner = this.project?.owner;
+    const members = this.project?.members;
+    if (owner?.email === user?.email || members?.some((member: Member) => member.email === user?.email)) {
+      this.isOwner = true;
+    }
+
   }
 }
