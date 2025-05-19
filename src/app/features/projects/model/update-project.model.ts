bff3351f86b3ccf0dd8f3efb8ca39e0eadeff81a
KevinKing66 <kevinvallenato2002@gmail.com>
2025-05-18 19:09:37 -0500

    Se arregla import

--- src/app/features/projects/model/update-project.model.ts
+++ src/app/features/projects/model/update-project.model.ts
@@ -1,15 +1,14 @@
-import { Member } from "./create-project.model";
+import { CreateEvidence, Member } from "./create-project.model";
 import { CreateFileDTO } from "./file.model";
 import { simpleResearchGroup } from "./simple-researchGroup.model";
-import { CreateEvidenceDto } from '../../../../../../../backend/scri-pro/src/evidences/dto/create-evidence.dto';
 
 export interface UpdateProject {
   code?: string | undefined;
   name?: string | undefined;
   image?: CreateFileDTO;
   members?: Member[] | undefined;
-  evidences?: CreateEvidenceDto[];
+  evidences?: CreateEvidence[];
   description?: string | undefined;
   status?: 'ACTIVE' | 'COMPLETED' | 'PAUSED' | undefined;
-  researchGroups?: { code: string; name: string } | undefined;
+  researchGroups?: simpleResearchGroup | undefined;
 }
